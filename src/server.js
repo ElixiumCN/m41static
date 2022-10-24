9bdf7cd05b375fc40445e9d263d481f4ccca3a79
ElixiumCN <100935972+ElixiumCN@users.noreply.github.com>
2022-10-24 11:53:24 +0100

    .
    .

--- src/server.js
+++ src/server.js
@@ -3,7 +3,11 @@ const app = express();
 
 app.use("/static", express.static("public")); //Translates the /static directory and repoints it to the public directory.
 // static is defining the route for the browser url
-
+// app.use("/contact", express.static("public/contact.html"));
+app.use("/static/myvideos", express.static("public/myvideos.html"));
+app.use("/static/contact", express.static("public/contact.html"));
+app.use("/static/login", express.static("public/login.html"));
+app.use("/static/notepad", express.static("public/notepad.html"));
 
 app.listen(5002, () => {
     console.log("Listening on Port 5002")
